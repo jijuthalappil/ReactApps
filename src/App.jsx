4d8e069a9910bb825746b51109bfb6a477090461
jijuthalappil <95568734+jijuthalappil@users.noreply.github.com>
2025-10-21 10:48:17 +0530

    crete and run server

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
+import Header from "./components/Header";
 export default function App() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/sign-in" element={<SignIn />} />
-				<Route path="/sign-up" element={<SignUp />} />
-				<Route path="/profile" element={<Profile />} />
-				<Route path="/about" element={<About />} />
-			</Routes>
+			<Header>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/sign-in" element={<SignIn />} />
+					<Route path="/sign-up" element={<SignUp />} />
+					<Route path="/profile" element={<Profile />} />
+					<Route path="/about" element={<About />} />
+				</Routes>
+			</Header>
 		</BrowserRouter>
 	);
 }
