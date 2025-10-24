b36b258efd51fb904fbe227042aebaac5c7e9f1b
jijuthalappil <95568734+jijuthalappil@users.noreply.github.com>
2025-10-24 07:15:09 +0530

    api with errorhandler

---
+++ api/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+	.connect(process.env.MONGO)
+	.then(() => {
+		console.log("connected to database ..");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+const app = express();
+app.use(express.json());
+app.listen(3000, () => {
+	console.log("Server is running on port 3000...");
+});
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, nex) => {
+	const statusCode = err.statusCode || 500;
+	const message = err.message || "Internal server error";
+	return res.status(statusCode).json({
+		sucess: false,
+		statusCode,
+		message,
+	});
+});
