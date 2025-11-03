6e32214956af2b858bdb802db37b8f97a3eaed58
jijuthalappil <95568734+jijuthalappil@users.noreply.github.com>
2025-11-03 17:19:42 +0530

    create sign in API

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
 export const signup = async (req, res, next) => {
 	const { username, email, password } = req.body;
 	const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -8,6 +12,25 @@ export const signup = async (req, res, next) => {
 	try {
 		await newUser.save();
 		res.status(201).json("User created successfully");
+	} catch (error) {
+		next(error);
+	}
+};
+export const signin = async (req, res, next) => {
+	const { email, password } = req.body;
+	try {
+		const validuser = await User.findOne({ email });
+		if (!validuser) {
+			return next(errorHandler(404, "User not find "));
+		}
+		const validpassword = bcryptjs.compareSync(password, validuser.password);
+		if (!validpassword) return next(errorHandler(401, "Wrong credential "));
+		const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET);
+		const { password: pass, ...rest } = validuser._doc;
+		res
+			.cookie("accesstoken", token, { httpOnly: true })
+			.status(200)
+			.json(rest);
 	} catch (error) {
 		next(error);
 	}
